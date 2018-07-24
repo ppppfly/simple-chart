dc63a57d544a150570a21b24732075645ea8cf32
MatthewPang <578658586@qq.com>
2018-07-25 07:26:19 +0800

    更新了 option 的获取方式

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -12,64 +12,9 @@ const InputGroup = Input.Group;
 class HomePage extends Component {
 
   state = {
-    option: {
-      title: {
-        text: '我的六力分析',
-        subtext: '多维度展现我的状态'
-      },
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend: {
-        x: 'center',
-        data: ['罗纳尔多', '舍普琴科']
-      },
-      toolbox: {
-        show: true,
-        feature: {
-          mark: {show: true},
-          dataView: {show: true, readOnly: false},
-          restore: {show: true},
-          saveAsImage: {show: true}
-        }
-      },
-      calculable: true,
-      polar: [
-        {
-          indicator: [
-            {text: '精力', max: 100},
-            {text: '专注力', max: 100},
-            {text: '执行力', max: 100},
-            {text: '习惯力', max: 100},
-            {text: '取舍力', max: 100},
-            {text: '梦想力', max: 100}
-          ],
-          radius: 130
-        }
-      ],
-      series: [
-        {
-          name: '完全实况球员数据',
-          type: 'radar',
-          itemStyle: {
-            normal: {
-              areaStyle: {
-                type: 'default'
-              }
-            }
-          },
-          data: [
-            {
-              value: [97, 42, 88, 94, 90, 86],
-              name: '舍普琴科'
-            },
-            {
-              value: [97, 32, 74, 95, 88, 92],
-              name: '罗纳尔多'
-            }
-          ]
-        }
-      ]
+    title: {
+      text: '我的六力分析',
+      subtext: '多维度展现我的状态'
     },
     attrs: [
       {text: '精力', max: 100},
@@ -78,6 +23,16 @@ class HomePage extends Component {
       {text: '习惯力', max: 100},
       {text: '取舍力', max: 100},
       {text: '梦想力', max: 100}
+    ],
+    data: [
+      {
+        value: [97, 42, 88, 94, 90, 86],
+        name: '舍普琴科'
+      },
+      {
+        value: [97, 32, 74, 95, 88, 92],
+        name: '罗纳尔多'
+      }
     ]
   };
 
@@ -89,10 +44,8 @@ class HomePage extends Component {
     e.preventDefault();
     this.props.form.validateFields((error, values) => {
       if (!error) {
-        let new_option = {...this.state.option};
         let {text, subtext} = values;
-        new_option.title = {text, subtext};
-        this.setState({option: new_option})
+        this.setState({title: {text, subtext}})
       } else {
         console.log('error', error, values);
       }
@@ -103,8 +56,7 @@ class HomePage extends Component {
     e.preventDefault();
     this.props.form.validateFields((error, values) => {
       if (!error) {
-        let new_option = {...this.state.option};
-        this.setState({option: new_option})
+        console.log('values', values);
       } else {
         console.log('error', error, values);
       }
@@ -113,22 +65,19 @@ class HomePage extends Component {
 
   deleteAttr = (group_id) => {
     // 获取当前 option
-    let new_option = {...this.state.option};
-    let indicators = this.state.option.polar[0].indicator.slice();
-    let data = this.state.option.series[0].data.slice();
+    let attrs = this.state.attrs;
+    let data = this.state.data;
 
     // 处理数据
     // --> 处理指标
-    indicators.splice(group_id, 1);
+    attrs.splice(group_id, 1);
     // --> 处理数值
     data.forEach((obj) => {
       obj.value.splice(group_id, 1);
     });
 
     // 更新 option
-    new_option.polar[0].indicator = indicators;
-    new_option.series[0].data = data;
-    this.setState({option: new_option});
+    this.setState({attrs, data});
   };
 
   getAttrsItems = () => {
@@ -171,11 +120,63 @@ class HomePage extends Component {
   };
 
 
+
+  getOption = () => {
+
+    let data_names = [];
+
+    for (let data of this.state.data) {
+      data_names.push(data['name'])
+    }
+
+    const option = {
+      title: this.state.title,
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        x: 'center',
+        data: data_names
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          mark: {show: true},
+          dataView: {show: true, readOnly: false},
+          restore: {show: true},
+          saveAsImage: {show: true}
+        }
+      },
+      calculable: true,
+      polar: [
+        {
+          indicator: this.state.attrs,
+          radius: 130
+        }
+      ],
+      series: [
+        {
+          name: '完全实况球员数据',
+          type: 'radar',
+          itemStyle: {
+            normal: {
+              areaStyle: {
+                type: 'default'
+              }
+            }
+          },
+          data: this.state.data
+        }
+      ]
+    }
+    console.log('option', option);
+    return option;
+  };
+
+
   render() {
 
     const { getFieldDecorator } = this.props.form;
-
-    console.log(this.state.option);
 
     return (
       <div className={container}>
@@ -186,7 +187,7 @@ class HomePage extends Component {
             <Form onSubmit={this.handleTitleChange}>
               <FormItem>
                 {getFieldDecorator('text', {
-                  initialValue: this.state.option.title.text,
+                  initialValue: this.state.title.text,
                   rules: [{ required: false }],
                 })(
                   <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="主标题"/>
@@ -194,7 +195,7 @@ class HomePage extends Component {
               </FormItem>
               <FormItem>
                 {getFieldDecorator('subtext', {
-                  initialValue: this.state.option.title.subtext,
+                  initialValue: this.state.title.subtext,
                   rules: [{ required: false }],
                 })(
                   <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="副标题"/>
@@ -220,7 +221,9 @@ class HomePage extends Component {
         <br />
 
         <ReactEcharts
-          option={this.state.option}
+          option={this.getOption()}
+          notMerge={true}
+          lazyUpdate={false}
           style={{
             width: '600px',
             height: '400px',
